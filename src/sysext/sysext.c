f14b3e3ac0f39fbaca9b11d6d178488af29ce916
Bardia Moshiri <bardia@furilabs.com>
2026-01-16 18:12:55 -0500

    Revert "tree-wide: normalize comment style"
    This reverts commit 93378148d48fa700c7f53b5f8969787af2a47ccb.

--- src/sysext/sysext.c
+++ src/sysext/sysext.c
@@ -187,9 +187,9 @@ static int parse_config_file(ImageClass image_class) {
                         sections,
                         config_item_table_lookup, items,
                         CONFIG_PARSE_WARN,
-                        /* userdata= */ NULL,
-                        /* ret_stats_by_path= */ NULL,
-                        /* ret_dropin_files= */ NULL);
+                        /* userdata = */ NULL,
+                        /* ret_stats_by_path = */ NULL,
+                        /* ret_dropin_files = */ NULL);
         if (r < 0)
                 return r;
 
@@ -313,7 +313,7 @@ static int need_reload(
                         const char *extension_reload_manager = NULL;
                         int b;
 
-                        r = load_extension_release_pairs(arg_root, image_class, *extension, /* relax_extension_release_check= */ true, &extension_release);
+                        r = load_extension_release_pairs(arg_root, image_class, *extension, /* relax_extension_release_check = */ true, &extension_release);
                         if (r < 0) {
                                 log_debug_errno(r, "Failed to parse extension-release metadata of %s, ignoring: %m", *extension);
                                 continue;
